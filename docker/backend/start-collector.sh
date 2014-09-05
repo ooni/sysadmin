21979a348cbfbd17db19318d9e6a0be5b0b39751
Arturo Filastò <art@fuffa.org>
2014-09-05 16:44:08 +0200

    Also create tor and archive subdirectories

--- docker/backend/start-collector.sh
+++ docker/backend/start-collector.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-mkdir -p /data/collector
+mkdir -p /data/collector/archive
+mkdir -p /data/collector/tor
 docker run -d --name="bridge-reachability-collector" -v /data/collector/:/data/ ooni/collector
