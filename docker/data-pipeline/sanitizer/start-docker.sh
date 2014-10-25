6731903a457f344be69ba2b74632fe9a11b745d4
David Stainton <dstainton415@gmail.com>
2014-10-25 17:38:16 +0000

    Clean up start wrapper script for sanitizer docker image

--- docker/data-pipeline/sanitizer/start-docker.sh
+++ docker/data-pipeline/sanitizer/start-docker.sh
@@ -1,6 +1,4 @@
 #!/bin/dash
 
 mkdir -p /data/raw
-#docker run -d --name="sanitizer" -v /data/raw/:/data/raw -v /data/sanitized:/data/sanitized -v /data/public-archive:/data/public-archive -e bridge_maping=/data/raw/path_to_ip_port_based_mapping.json ooni/sanitizer
-docker run -d --name="sanitizer" -v /data/raw:/data/raw -v /data/sanitized:/data/sanitized -v /data/public-archive:/data/sanitized-archive ooni/sanitizer
-
+docker run -d --name="sanitizer" -v /data/raw:/data/raw -v /data/sanitized:/data/sanitized -v /data/sanitized-archive:/data/sanitized-archive ooni/sanitizer
