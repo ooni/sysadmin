94128c11e8acbdb3ee7563674b599ef4aefb8fa8
David Stainton <dstainton415@gmail.com>
2014-10-26 13:19:40 +0000

    Teach import-docker to use new ooni pipline cli

--- docker/data-pipeline/importer/start-docker.sh
+++ docker/data-pipeline/importer/start-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/dash
+. ./config.sh
 
-docker run -d --name="importer" -v /data/sanitized:/data/sanitized -v /data/public:/data/public
+docker run -d --name="importer" -v $OONI_SANITISED_DIR:$OONI_SANITISED_DIR -v $OONI_PUBLIC_DIR:$OONI_PUBLIC_DIR --env-file=./config.sh ooni/import
