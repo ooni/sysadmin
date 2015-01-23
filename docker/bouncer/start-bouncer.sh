4c1af0f192e12878239019ce681428b7b5e91f3f
Arturo Filastò <art@fuffa.org>
2015-01-23 15:59:08 +0100

    Finalize the canonical bouncer docker container

--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /data/bouncer/etc
 mkdir -p /data/bouncer/var/log/ooni
-docker run -d --name="ooni-canonical-bouncer" -v /data/bouncer/etc:/etc/ -v /data/bouncer/var/log/ooni:/var/log/ooni -v /data/bouncer/:/var/spool/ooni/backend/ ooni/bouncer
+docker run -d --name="ooni-canonical-bouncer" -v /data/bouncer/var/log/ooni:/var/log/ooni -v /data/bouncer/:/var/spool/ooni/backend/ ooni/bouncer
