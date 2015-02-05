fd546f0292c3a5df1462d479d24c2120a8c0c303
Arturo Filastò <art@fuffa.org>
2015-02-05 17:20:11 +0100

    Share private directory in sanitiser

--- docker/data-pipeline/sanitiser/start.sh
+++ docker/data-pipeline/sanitiser/start.sh
@@ -2,4 +2,4 @@
 
 . ./config.sh
 
-docker run -d --name="sanitiser" -v $OONI_RAW_DIR:$OONI_RAW_DIR -v $OONI_SANITISED_DIR:$OONI_SANITISED_DIR -v $OONI_ARCHIVE_DIR:$OONI_ARCHIVE_DIR ooni/sanitiser
+docker run -d --name="sanitiser" -v /data/private:/data/private -v $OONI_RAW_DIR:$OONI_RAW_DIR -v $OONI_SANITISED_DIR:$OONI_SANITISED_DIR -v $OONI_ARCHIVE_DIR:$OONI_ARCHIVE_DIR ooni/sanitiser
