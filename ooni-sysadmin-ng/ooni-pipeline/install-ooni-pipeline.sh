f99e7c9fff4dc2dcce224af71ec3236d6eacbb21
Joe Landers <joe@joelanders.net>
2015-12-10 22:40:21 +0100

    ooni-pipeline fixes

--- ooni-sysadmin-ng/ooni-pipeline/install-ooni-pipeline.sh
+++ ooni-sysadmin-ng/ooni-pipeline/install-ooni-pipeline.sh
@@ -1,23 +1,28 @@
 yum upgrade -y
 yum -y groupinstall "Development Tools" "Development Libraries"
-yum install git wget python-devel screen \
+yum install -y git wget python-devel screen \
     postgresql-devel libyaml-devel htop vim
 
 wget "https://bootstrap.pypa.io/get-pip.py"
 python get-pip.py
 
-pip install virtualenvwrapper psycopg2
+pip install virtualenvwrapper
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7" >> /home/centos/.bashrc
-echo "export WORKON_HOME=$HOME/virtualenvs" >> /home/centos/.bashrc
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/centos/.bashrc
+echo "export WORKON_HOME=/home/centos/virtualenvs" >> /home/centos/.bashrc
+echo "source /usr/bin/virtualenvwrapper.sh" >> /home/centos/.bashrc
 source /home/centos/.bashrc
 mkvirtualenv ooni-pipeline
 
 git clone https://github.com/thetorproject/ooni-pipeline
-pip install -r requirements-dev.txt
+pip install -r ooni-pipeline/requirements-dev.txt
+pip install psycopg2
 
 # specify aws and postgres credentials
 sh _invoke.yaml.sh > ooni-pipeline/invoke.yaml
 
+# run as another user eventually
+chown -R centos ooni-pipeline
+
 # probably make this configurable by the ansible user
+cd ooni-pipeline
 invoke add_headers_to_db --date-interval 2012
