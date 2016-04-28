7e3f86a9994442146efc99707f3c084a392ae682
anadahz <kojgelo@inbox.com>
2016-04-28 13:11:52 -0300

    Create all backend required directories
    The backend will fail to start if not all directories are present.
    This fix creates all required directories so that they can mapped as
    volumes by docker

--- docker/backend/start-collector.sh
+++ docker/backend/start-collector.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mkdir -p /data/collector/archive
-mkdir -p /data/collector/tor
+mkdir -p /data/collector/{archive,tor,decks,inpurs,raw_reports,etc,var/log/ooni}
 docker run -d --name="bridge-reachability-collector" -v /data/collector/:/data/ ooni/collector
--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mkdir -p /data/bouncer/etc
-mkdir -p /data/bouncer/var/log/ooni
+mkdir -p /data/bouncer/{archive,tor,decks,inpurs,raw_reports,etc,var/log/ooni}
 docker run -d --name="ooni-canonical-bouncer" -v /data/bouncer/var/log/ooni:/var/log/ooni -v /data/bouncer/:/var/spool/ooni/backend/ -v /data/bouncer/inputs/:/usr/share/ooni/backend/inputs -v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer
