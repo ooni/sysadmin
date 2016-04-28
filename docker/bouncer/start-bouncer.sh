61b34256b5874b63725fb7186f3ce6c01bb0642b
anadahz <kojgelo@inbox.com>
2016-04-28 13:16:48 -0300

    Fix add correct backend PATH, improve visibility

--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 mkdir -p /data/bouncer/{archive,tor,decks,inpurs,raw_reports,etc,var/log/ooni}
-docker run -d --name="ooni-canonical-bouncer" -v /data/bouncer/var/log/ooni:/var/log/ooni -v /data/bouncer/:/var/spool/ooni/backend/ -v /data/bouncer/inputs/:/usr/share/ooni/backend/inputs -v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer
+docker run -d --name="ooni-canonical-bouncer" \
+	-v /data/bouncer/var/log/ooni:/var/log/ooni \
+	-v /data/bouncer/:/usr/share/ooni/backend/ \
+	-v /data/bouncer/inputs/:/usr/share/ooni/backend/inputs \
+	-v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer
