e7217dc7702f1aa7fe74dd2f0937050707c0c84a
anadahz <kojgelo@inbox.com>
2016-05-01 14:42:18 -0300

    Fix dir name, verbose mode, run in BASH shell
    Fix typo in dir name, run mkdir in verbose and change to BASH shell for
    shell expansion to work.

--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-mkdir -p /data/bouncer/{archive,tor,decks,inpurs,raw_reports,etc,var/log/ooni}
+#!/bin/bash
+mkdir -pv /data/bouncer/{archive,tor,decks,inputs,raw_reports,etc,var/log/ooni}
 docker run -d --name="ooni-canonical-bouncer" \
 	-v /data/bouncer/var/log/ooni:/var/log/ooni \
 	-v /data/bouncer/:/usr/share/ooni/backend/ \
