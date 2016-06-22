c02cc020056f47099faa7eba6ce332c1fcc7ca3a
anadahz <andz@torproject.org>
2016-06-22 20:10:51 -0300

    Fix conditionals, use newer TLS configuration format
    * Fix template ansible conditionals
    * Use chain and private key for TLS test-helpers, bouncer and collector
    * Update the docker bouncer startup script
    * Remove task to populate oonibackend and bouncer configuration
    * Move oonibackend.conf in a location where we can edit without entering
    the docker container

--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-mkdir -pv /data/bouncer/{archive,tor,decks,inputs,raw_reports,etc,var/log/ooni}
-docker run -d --name="ooni-canonical-bouncer" \
+mkdir -pv \
+  /data/bouncer/{tls,archive,tor,decks,inputs,raw_reports,etc/ooni,var/log/ooni}
+docker run --net=host -d --name="ooni-canonical-bouncer" \
 	-v /data/bouncer/var/log/ooni:/var/log/ooni \
 	-v /data/bouncer/:/usr/share/ooni/backend/ \
 	-v /data/bouncer/inputs/:/usr/share/ooni/backend/inputs \
-	-v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer
+	-v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer \
+	-v /data/bouncer/etc/ooni:/etc/ooni
