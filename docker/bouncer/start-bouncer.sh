3dfdfdc5d3f62ea6dbdd0698f346e3c6338eaa37
anadahz <andz@torproject.org>
2016-06-23 00:58:52 -0300

    Fix volume option

--- docker/bouncer/start-bouncer.sh
+++ docker/bouncer/start-bouncer.sh
@@ -5,5 +5,5 @@ docker run --net=host -d --name="ooni-canonical-bouncer" \
 	-v /data/bouncer/var/log/ooni:/var/log/ooni \
 	-v /data/bouncer/:/usr/share/ooni/backend/ \
 	-v /data/bouncer/inputs/:/usr/share/ooni/backend/inputs \
-	-v /data/bouncer/decks:/usr/share/ooni/backend/decks ooni/bouncer \
-	-v /data/bouncer/etc/ooni:/etc/ooni
+	-v /data/bouncer/decks:/usr/share/ooni/backend/decks \
+	-v /data/bouncer/etc/ooni:/etc/ooni ooni/bouncer
