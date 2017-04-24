9576891eca7c350188a5e345e01b220429159b72
Leonid Evdokimov <leon@darkk.net.ru>
2017-04-24 15:14:05 +0300

    airflow: fix some typos

--- ansible/roles/airflow/files/airflow-dags/shovel_jump.sh
+++ ansible/roles/airflow/files/airflow-dags/shovel_jump.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 exec sudo --non-interactive /usr/local/bin/docker-trampoline \
-    "{{ task_id }}" "{{ ds }}" "{{ execution_date.isoformat() }}" "{{ (execution_date + dag.schedule_interval).isoformat() }}"
+    "{{ task.task_id }}" "{{ ds }}" "{{ execution_date.isoformat() }}" "{{ (execution_date + dag.schedule_interval).isoformat() }}"
