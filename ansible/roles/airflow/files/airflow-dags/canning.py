255081f35d42c300b524d66eef0e704fedb03a17
Leonid Evdokimov <leon@darkk.net.ru>
2017-07-14 01:43:09 +0300

    airflow: pipeline ticking every 24h
    It's delayed by 24h compared to luigi pipeline as ${bucket_date} changed
    it meaning some time ago and luigi and airflow are running side by side
    for a while.

--- ansible/roles/airflow/files/airflow-dags/canning.py
+++ ansible/roles/airflow/files/airflow-dags/canning.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 class ReportsRawReadySensor(BaseSensorOperator):
     def poke(self, context):
-        retcode = subprocess.call(['sudo', '--non-interactive', '/usr/local/bin/docker-trampoline', 'is-reports-raw-ready',
+        retcode = subprocess.call(['sudo', '--non-interactive', '/usr/local/bin/docker-trampoline', 'reports_raw_sensor',
             context['ds'], context['execution_date'].isoformat(), (context['execution_date'] + context['dag'].schedule_interval).isoformat()])
         if retcode == 42:
             return True
@@ -21,7 +21,7 @@ dag = DAG(
     dag_id='hist_canning',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2012, 12, 5),
-    end_date=datetime(2017, 7, 6), # NB: end_date is included
+    #end_date=datetime(2017, 7, 7), # NB: end_date is included
     default_args={
         'email': 'leon+airflow@darkk.net.ru',
         'retries': 1,
@@ -30,6 +30,7 @@ dag = DAG(
 # NB: removing an Operator from DAG leaves some trash in the database tracking
 # old state of that operator, but it seems to trigger no issues with 1.8.0
 
+ReportsRawReadySensor(task_id='reports_raw_sensor', poke_interval=5*60, timeout=12*3600, dag=dag)
 BashOperator(pool='datacollector_disk_io', task_id='canning', bash_command='shovel_jump.sh', dag=dag)
 BashOperator(pool='datacollector_disk_io', task_id='autoclaving', bash_command='shovel_jump.sh', dag=dag)
 BashOperator(pool='datacollector_disk_io', task_id='meta_pg', bash_command='shovel_jump.sh', dag=dag)
@@ -38,6 +39,8 @@ BashOperator(pool='datacollector_disk_io', task_id='reports_raw_cleanup', bash_c
 BashOperator(pool='datacollector_disk_io', task_id='sanitised_s3_ls', bash_command='shovel_jump.sh', dag=dag)
 BashOperator(pool='datacollector_disk_io', task_id='sanitised_check', bash_command='shovel_jump.sh', dag=dag)
 BashOperator(pool='datacollector_disk_io', task_id='sanitised_cleanup', bash_command='shovel_jump.sh', dag=dag)
+
+dag.set_dependency('reports_raw_sensor', 'canning')
 
 dag.set_dependency('canning', 'autoclaving')
 dag.set_dependency('autoclaving', 'meta_pg')
