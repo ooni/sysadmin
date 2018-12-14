9b5ded8447cc0d64fe6cfc2f57a0ada74a0941ec
Leonid Evdokimov <leon@darkk.net.ru>
2018-12-14 18:23:26 +0300

    airflow: drop email alerts, Prometheus + Alertmanager do better job

--- ansible/roles/airflow/files/airflow-dags/sync.py
+++ ansible/roles/airflow/files/airflow-dags/sync.py
@@ -9,7 +9,6 @@ with DAG(
     start_date=datetime(2018, 10, 21),
     catchup=False,
     default_args={
-        'email': 'leonid@openobservatory.org', # prometheus/alertmanager sends to team@ but airflow is more chatty
         'retries': 0,
     }) as fetcher:
 
