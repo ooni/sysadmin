9afc58579520074c4071d161a8614505b527b4f9
Federico Ceratto <federico@openobservatory.org>
2020-01-16 17:35:06 +0000

    Use bash trap, add logging

--- ansible/roles/th-web-connectivity/templates/if-nok-restart.sh
+++ ansible/roles/th-web-connectivity/templates/if-nok-restart.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#
+# Simple liveness check for wcth
+# Logs can be retrieved with sudo journalctl -f --identifier=wcth-monitor
+#
+set -euo pipefail
+restart() {
+  logger -t wcth-monitor "test-helper liveness check failed"
+  systemctl restart web-connectivity-th.service
+}
 
-if curl -f -m 5 -s {{ web_connectivity_ipv4 }}:{{ web_connectivity_port }}/status | grep '{"status": "ok"}' > /dev/null
-then exit 0
-else
-    systemctl restart web-connectivity-th.service
-fi
+trap restart SIGPIPE ERR
+
+url="{{ web_connectivity_ipv4 }}:{{ web_connectivity_port }}/status"
+logger "checking test-helper at $url"
+logger -t wcth-monitor "checking wcth at $url"
+curl -f -m 5 -s "$url" | grep -q '{"status": "ok"}'
