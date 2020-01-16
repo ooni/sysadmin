ab1ceb167c43d95acda0a29e809de1b25155ca0f
Arturo Filastò <arturo@filasto.net>
2020-01-16 09:39:55 +0100

    Fail early curl flag

--- ansible/roles/th-web-connectivity/templates/if-nok-restart.sh
+++ ansible/roles/th-web-connectivity/templates/if-nok-restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if curl -m 5 -s {{ web_connectivity_ipv4 }}:{{ web_connectivity_port }}/status | grep '{"status": "ok"}' > /dev/null
+if curl -f -m 5 -s {{ web_connectivity_ipv4 }}:{{ web_connectivity_port }}/status | grep '{"status": "ok"}' > /dev/null
 then exit 0
 else
     systemctl restart web-connectivity-th.service
