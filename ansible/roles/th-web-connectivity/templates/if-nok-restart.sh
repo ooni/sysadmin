71c0ae155ca9e7df2b3db40c2ecd5003f17d3644
Arturo Filastò <arturo@filasto.net>
2020-01-16 09:30:02 +0100

    Add very basic monitoring and restarting of the web-connectivity test helper service

---
+++ ansible/roles/th-web-connectivity/templates/if-nok-restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if curl -m 5 -s {{ web_connectivity_ipv4 }}:{{ web_connectivity_port }}/status | grep '{"status": "ok"}' > /dev/null
+then exit 0
+else
+    systemctl restart web-connectivity-th.service
+fi
