78724c519a73741124f5b367b2453e75a740fc89
Federico Ceratto <federico@debian.org>
2022-10-25 11:53:50 +0100

    Remove airflow role and few other files

--- ansible/roles/airflow/files/airflow-dags/shovel_jump.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec sudo --non-interactive /usr/local/bin/docker-trampoline "{{ task.task_id }}" "{{ ds }}" "{{ execution_date.isoformat() }}" "{{ (execution_date + dag.schedule_interval).isoformat() }}" {{ params.argv | default([]) | join(' ') }}
