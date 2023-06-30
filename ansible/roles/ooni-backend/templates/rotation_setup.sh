1e59eb871b2e817228119bcc0c7259b2beb321f1
Federico Ceratto <federico@debian.org>
2023-06-30 19:39:16 +0100

    Install Vector in the TH rotation script

--- ansible/roles/ooni-backend/templates/rotation_setup.sh
+++ ansible/roles/ooni-backend/templates/rotation_setup.sh
@@ -2,7 +2,7 @@
 # Managed by ansible, see roles/ooni-backend/tasks/main.yml
 #
 # Configure test-helper droplet
-# This script is run remotely on newly spawned VM by https://github.com/ooni/pipeline/blob/master/af/analysis/rotation.py
+# This script is run remotely on newly spawned VM by https://github.com/ooni/backend/blob/master/analysis/rotation.py
 # It runs as root and with CWD=/
 #
 set -euo pipefail
@@ -28,12 +28,57 @@ oN7jfdbznrWVigE=
 -----END PGP PUBLIC KEY BLOCK-----
 EOF
 
+# Vector
+cat <<EOF  | gpg --dearmor > /etc/apt/trusted.gpg.d/vector.gpg
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v2
+
+mQENBF9gFZ0BCADETtIHM8y5ehMoyNiZcriK+tHXyKnbZCKtMCKcC4ll94/6pekQ
+jKIPWg8OXojkCtwua/TsddtQmOhUxAUtv6K0jO8r6sJ8rezMhuNH8J8rMqWgzv9d
+2+U7Z7GFgcP0OeD+KigtnR8uyp50suBmEDC8YytmmbESmG261Y38vZME0VvQ+CMy
+Yi/FvKXBXugaiCtaz0a5jVE86qSZbKbuaTHGiLn05xjTqc4FfyP4fi4oT2r6GGyL
+Bn5ob84OjXLQwfbZIIrNFR10BvL2SRLL0kKKVlMBBADodtkdwaTt0pGuyEJ+gVBz
+629PZBtSrwVRU399jGSfsxoiLca9//c7OJzHABEBAAG0OkNsb3Vkc21pdGggUGFj
+a2FnZSAodGltYmVyL3ZlY3RvcikgPHN1cHBvcnRAY2xvdWRzbWl0aC5pbz6JATcE
+EwEIACEFAl9gFZ0CGy8FCwkIBwMFFQoJCAsFFgIDAQACHgECF4AACgkQNUPbLQor
+xLhf6gf8DyfIpKjvEeW/O8lRUTpkiPKezJbb+udZboCXJKDD02Q9PE3hfEfQRr5X
+muytL7YMPvzqBVuP3xV5CN3zvtiQQbZiDhstImVyd+t24pQTkjzkvy+A2yvUuIkE
+RWxuey41f5FNj/7wdfJnHoU9uJ/lvsb7DLXw7FBMZFNBR6LED/d+b61zMzVvmFZA
+gsrCGwr/jfySwnpShmKdJaMTHQx0qt2RfXwNm2V6i900tAuMUWnmUIz5/9vENPKm
+0+31I43a/QgmIrKEePhwn2jfA1oRlYzdv+PbblSTfjTStem+GqQkj9bZsAuqVH8g
+3vq0NvX0k2CLi/W9mTiSdHXFChI15A==
+=k36w
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+
+echo "deb https://repositories.timber.io/public/vector/deb/debian bullseye main" > /etc/apt/sources.list.d/vector.list
+
 echo "Installing packages"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -q
 apt-get purge -qy unattended-upgrades rsyslog
 apt-get upgrade -qy
-apt-get install -qy --no-install-recommends chrony netdata oohelperd netdata-plugins-python
+apt-get install -qy --no-install-recommends chrony netdata oohelperd netdata-plugins-python vector
+
+echo "Configuring Vector"
+# The certs are copied over by rotation.py
+cat > /etc/vector/vector.toml <<EOF
+[sources.local_journald]
+type = "journald"
+
+[sinks.monitoring_ooni_org]
+type = "vector"
+inputs = [ "local_journald" ]
+address = "monitoring.ooni.org:10514"
+compression = true
+
+tls.enabled = true
+tls.verify_certificate = true
+tls.verify_hostname = false
+tls.ca_file = "/etc/vector/oonicacert.pem"
+tls.crt_file = "/etc/vector/node-cert.pem"
+tls.key_file = "/etc/vector/node.key"
+EOF
 
 echo "Enabling incoming connections to Netdata"
 cat > /etc/netdata/netdata.conf <<EOF
